4f5fbe72636e3e6ec6c1dab028cca11244cbd47c
Raphael Zimmermann <mail@raphael.li>
2016-10-27 21:57:48 +0200

    Proof-of-concept: MathJax formula rendering

--- raphidoc/mdx_math.py
+++ raphidoc/mdx_math.py
@@ -1,7 +1,9 @@
 import os
-import hashlib
+import base64
 import logging
+import subprocess
 import re
+import json
 
 from markdown.inlinepatterns import Pattern
 from markdown.blockprocessors import BlockProcessor
@@ -10,81 +12,100 @@ from markdown.extensions import Extension
 import markdown
 
 from .exceptions import RaphidocException
-"""
-Math extension for Python-Markdown
-
-Based on http://freewisdom.org/projects/python-markdown/mdx_math
-
-Works only on *nix systems. Latex must be installed
-
-TODO: Add support for multi line <math></math> tags
 
 """
-BEFORE_TEX = """\\documentclass[12pt]{article}
-\\usepackage[latin1]{inputenc}
-\\usepackage{amsmath}
-\\usepackage{amsfonts}
-\\usepackage{amssymb}
-\\pagestyle{empty}
-\\begin{document}
-"""
-
-AFTER_TEX = """
-\\end{document}
+Math extension for Python-Markdown using Node & MathJax.
 """
 
 logger = logging.getLogger(__name__)
 
+# Global - very ugly, but used to improve performace
+INSTALLED = None
 
-def generateImage(code, mode, output_directory, destination):
-    digest = hashlib.md5(str(mode + code).encode('utf-8')).hexdigest()
-    directory = os.path.join(output_directory, destination)
 
-    png = os.path.join(directory, digest + '.png')
-    png_link = os.path.join(destination, digest + '.png')
-    tex = os.path.join(directory, digest + '.tex')
+def node_installed():
+    """
+    Checks wheather there the programms "node" and "npm" are in the path.
+    If so, returns True - otherwise False.
+    """
+    node = False
+    npm = False
+    for path in os.environ["PATH"].split(os.pathsep):
+        path = path.strip('"')
+        if os.path.exists(os.path.join(path, 'npm')):
+            npm = True
+        if os.path.exists(os.path.join(path, 'node')):
+            node = True
+        if node and npm:
+            return True
+    return False
 
-    # if the image already exists
-    if os.path.exists(png):
-        return png_link
 
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+def run_in_node(program):
+    """
+    Runs the given program in node and returns *only* it's stdout.
+    Any errors will be ignored (except exceptions)
+    """
+    p1 = subprocess.Popen('node',
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE)
 
-    with open(tex, "w") as f:
-        f.writelines([BEFORE_TEX, code, AFTER_TEX])
-        f.close()
+    stdout, stderr = p1.communicate(program.encode())
+    return stdout.decode()
 
-    compile_latex = ('latex -interaction=batchmode -output-directory={d}'
-                     ' {t} > /dev/null'.format(d=directory, t=tex))
-    if os.system(compile_latex) != 0:
-        raise RaphidocException('Failed to compile latex')
-    latex2png = 'dvipng -D 250 -o {d}/{f}.png -T tight {d}/{f}.dvi > /dev/null'.format(
-                 d=directory, f=digest)
-    # TODO: Capture stdout & log on error!
-    if os.system(latex2png) != 0:
-        raise RaphidocException('Failed to converte to png :(')
 
-    # clean up
-    os.remove(tex)
-    os.remove(os.path.join(directory, digest + '.log'))
-    os.remove(os.path.join(directory, digest + '.dvi'))
-    os.remove(os.path.join(directory, digest + '.aux'))
-    return png_link
+def install_dependencies():
+    # TODO: Run in CWD
+    # TODO: shortcut: if CWD contains a folder node_modules/mathjax-node, log info and skip
+    global INSTALLED
+    if not node_installed():
+        INSTALLED = False
+        return INSTALLED
+
+    # ret = subprocess.call(['npm', 'install', 'mathjax-node'], timeout=50)
+    # INSTALLED = ret == 0
+    # return INSTALLED
+    INSTALLED = True
+    return INSTALLED
+
+
+def compile_latex(formula, inline=False):
+    """
+    Compiles the given Tex formula and returns it as a SVG.
+    If the formula is invalid, an SVG is returned as well - but it contains an error message.
+    (See MathJax for details)
+    """
+    if INSTALLED is None:
+        install_dependencies()
+
+    if not INSTALLED:
+        logger.warning("MathJax or Node is not installed!")
+        return formula
+
+    program = """var mjAPI = require("mathjax-node/lib/mj-single.js");
+    mjAPI.typeset({
+      math: %s,
+      format: "%s", // "TeX", "inline-TeX", "MathML"
+      svg:true
+    }, function (data) {
+        console.log(data.svg);
+    });
+    """ % (json.dumps(formula), "inline-TeX" if inline else "TeX")
+    return run_in_node(program)
+
+
+def base64_image_url(svg):
+    return 'data:image/svg+xml;charset=utf-8;base64,' + base64.b64encode(svg.encode()).decode()
 
 
 class MathInlinePattern(markdown.inlinepatterns.Pattern):
 
-    def __init__(self, output_directory, destination):
-        markdown.inlinepatterns.Pattern.__init__(self, r'\$\$(((?!\$\$).)*)\$\$')
-
-        self.output_directory = output_directory
-        self.destination = destination
-
     def handleMatch(self, m):
+        # TODO: Allow to fully inline SVG - via config?
         node = etree.Element('img')
         code = m.group(2).strip()
-        node.set('src', generateImage('${0}$'.format(code), 'inline', self.output_directory, self.destination))
+        node.set('src', base64_image_url(compile_latex(code, True)))
         node.set('alt', code)
         node.set('class', 'inline-math')
         return node
@@ -94,12 +115,6 @@ class MathBlockProcessor(BlockProcessor):
 
     RE = re.compile(r'(\\\[)(?P<formula>\n(.*\n?)*)(\\\])')
 
-    def __init__(self, parser, output_directory, destination):
-        BlockProcessor.__init__(self, parser)
-
-        self.output_directory = output_directory
-        self.destination = destination
-
     def test(self, parent, block):
         return bool(self.RE.fullmatch(block))
 
@@ -107,8 +122,7 @@ class MathBlockProcessor(BlockProcessor):
         raw_block = blocks.pop(0)
         code = self.RE.search(raw_block).group('formula')
         node = etree.Element("img")
-        node.set('src', generateImage('\\[\{0}\\]'.format(code), 'inline',
-                 self.output_directory, self.destination))
+        node.set('src', base64_image_url(compile_latex(code, False)))
         node.set('alt', code)
         node.set('class', 'block-math')
         parent.append(node)
@@ -116,18 +130,16 @@ class MathBlockProcessor(BlockProcessor):
 
 class MathExtension(markdown.Extension):
 
-    def __init__(self, output_directory, destination='assets'):
+    def __init__(self, output_directory):
         markdown.Extension.__init__(self)
         self.output_directory = output_directory
-        self.destination = destination
 
     def extendMarkdown(self, md, md_globals):
         md.inlinePatterns.add('inlinemath',
-                              MathInlinePattern(self.output_directory, self.destination),
+                              MathInlinePattern(r'\$\$(((?!\$\$).)*)\$\$'),
                               '<escape')
         md.parser.blockprocessors.add('blockmath',
-                                      MathBlockProcessor(md.parser, self.output_directory,
-                                                         self.destination), '_begin')
+                                      MathBlockProcessor(md.parser), '_begin')
 
 
 def makeExtension(**kwargs):
